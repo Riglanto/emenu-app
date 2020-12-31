2157ed0ec01204c9501bf38eb6184c71aa34afc2
Riglanto <jaaroos@gmail.com>
2020-12-31 17:02:55 +0100

    Labels fixed

--- i18n.ts
+++ i18n.ts
@@ -1,15 +1,15 @@
-import NextI18Next from 'next-i18next';
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
-import path from 'path';
+import NextI18Next from "next-i18next";
+const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
+import path from "path";
 
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== "production";
 
 const i18n = new NextI18Next({
-	defaultLanguage: 'en',
-	otherLanguages: [],
-	debug,
-	localeSubpaths: localeSubpaths as Record<string, string>,
-	localePath: path.resolve('./public/locales')
+  defaultLanguage: "en",
+  otherLanguages: [],
+  debug,
+  localeSubpaths: localeSubpaths as Record<string, string>,
+  localePath: path.resolve("./public/locales"),
 });
 
 export const { Trans, Link, useTranslation } = i18n;
